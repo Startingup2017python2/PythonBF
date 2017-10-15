2c23b10a97ad4f8bc7f9da482b65604e25dfff53
Mostafa Ahangarha <ahangarha@gmail.com>
2017-10-16 03:00:23 +0330

    Updating the BF code to my own BF code from previus homework, and enhacing the main program's functionality

--- ahangarha.py
+++ ahangarha.py
@@ -13,6 +13,8 @@ cell = {}   # Cells as ASCII code container
 
 pointer = 0 # Pointer to determine which cell is under operation
 
+outputString = "" # Container or the output of the program
+
 # Reading code from the code file
 with open("./ahangarha-code.bf", "r") as codeFile:
     for l in codeFile:
@@ -39,36 +41,69 @@ def dec():
         cell[pointer] = 0
     cell[pointer] = cell[pointer] - 1
 
-#shift right (>)
+# shift right (>)
 def shift_right():
     global pointer
     pointer = pointer + 1
 
-#shift right (<)
+# shift right (<)
 def shift_left():
     global pointer
     pointer = pointer - 1
 
-#output (.)
+# output (.)
 def output():
-    global cell, pointer
+    global cell, pointer, outputString
     
     if cell.get(pointer) is None:
         cell[pointer] = 0
     
-    print(chr(cell[pointer]))
+    #print(chr(cell[pointer]))
+    outputString += chr(cell[pointer])
 
+
+# opening loop ([)
+def open_loop():
+    global cell, pointer, i
+    
+    if cell[pointer]==0:
+        
+        while code[i]!="]":
+            i += 1
+            
+        
+
+# closing loop (])
+def close_loop():
+    global cell, pointer, i
+    
+    if cell[pointer]!=0:
+        
+        while code[i]!="[":
+            i -= 1
+    
 # =================================
 # Running the BF code
 
-for c in code:
-    if c=="+":
-        inc()
-    elif c =="-":
-        dec()
-    elif c ==">":
-        shift_right()
-    elif c =="<":
-        shift_left()
-    elif c ==".":
-        output()
+# i as counter
+i = 0
+
+while i < len(code):
+    # map the inputs to the function blocks
+    operate = {
+       '+' : inc,
+       '-' : dec,
+       '>' : shift_right,
+       '<' : shift_left,
+       '.' : output,
+       '[' : open_loop,
+       ']' : close_loop,
+    }
+    
+    # c as charater
+    c = code[i]
+    operate[c]()
+    
+    i += 1
+
+print(outputString)
